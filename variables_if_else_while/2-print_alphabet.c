88b2d50ea4a212ccb5c66defed524f746f552ae5
J-D95 <6252@holbertonstudents.com>
2023-02-23 22:13:54 -0800

    2-print_alphabet.c

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Write a program that prints the alphabet in lowercase,
+ * followed by a new line
+ *
+ * Return (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
